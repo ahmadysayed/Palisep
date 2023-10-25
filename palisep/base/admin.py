2f759fda8c59a6da6926b1391e7ea8252b4dde97
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-10-25 15:32:26 +0200

    Database Model

--- palisep/base/admin.py
+++ palisep/base/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Admin)
+admin.site.register(Armorial)
+admin.site.register(LegentPhotos)
+admin.site.register(Details)
+admin.site.register(Categories)
+admin.site.register(Shots)
+admin.site.register(MyReferences)
+admin.site.register(Partnaire)
+admin.site.register(Patronyme)
+admin.site.register(Equivalence)
+admin.site.register(Presentation)
+admin.site.register(Blasonnements)
