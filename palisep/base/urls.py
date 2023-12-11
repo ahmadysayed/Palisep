8d5a51b0032e467c9b4379183511045426022f08
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-12-11 22:22:39 +0100

    CRUD APIs

--- palisep/base/urls.py
+++ palisep/base/urls.py
@@ -10,6 +10,11 @@ urlpatterns = [
     path('patronyme/', views.getPatronyme, name='patronyme'),
     path('partnaire/', views.getPartnaire, name='partnaire'),
     path('genealogy/', views.getGenealogy, name='genealogy'),
+    path('reference/', views.getReferences, name='reference'),
+    path('shot/', views.getShots, name='shot'),
+    path('detail/', views.getDetails, name='detail'),
+    path('legent-photo/', views.getLegentPhotos, name='legent-photo'),
+    path('armorial/', views.getArmorial, name='armorial'),
 
     path('blasonnement-delete/<str:pk>/', views.deleteBlasonnement, name="blasonnement-delete"),
     path('presentation-delete/<str:pk>/', views.deletePresentation, name="presentation-delete"),
@@ -24,5 +29,36 @@ urlpatterns = [
     path('lengentphotos-delete/<str:pk>/', views.deleteLegentPhotos, name="lengentphotos-delete"),
     path('armorial-delete/<str:pk>/', views.deleteArmorial, name="armorial-delete"),
     path('user-delete/<str:pk>/', views.deleteAdmin, name="user-delete"),
+
+
+    path('api/blasonnements/create/', views.createBlasonnements, name='create_blasonnements'),
+    path('api/categories/create/', views.createCategories, name='create_categories'),
+    path('api/presentation/create/', views.createPresentation, name='create_presentation'),
+    path('api/equivalence/create/', views.createEquivalence, name='create_equivalence'),
+    path('api/patronyme/create/', views.createPatronyme, name='create_patronyme'),
+    path('api/partnaire/create/', views.createPartnaire, name='create_partnaire'),
+    path('api/genealogy/create/', views.createGenealogy, name='create_genealogy'),
+    path('api/references/create/', views.createReferences, name='create_references'),
+    path('api/shots/create/', views.createShots, name='create_shots'),
+    path('api/details/create/', views.createDetails, name='create_details'),
+    path('api/legentphotos/create/', views.createLegentPhotos, name='create_legentphotos'),
+    path('api/armorial/create/', views.createArmorial, name='create_armorial'),
+
+
+    path('api/armorial/update/<int:pk>/', views.updateArmorial, name='update_armorial'),
+    path('api/legentphotos/update/<int:pk>/', views.updateLegentPhotos, name='update_legentphotos'),
+    path('api/details/update/<int:pk>/', views.updateDetails, name='update_details'),
+    path('api/categories/update/<int:pk>/', views.updateCategories, name='update_categories'),
+    path('api/shots/update/<int:pk>/', views.updateShots, name='update_shots'),
+    path('api/myreferences/update/<int:pk>/', views.updateMyReferences, name='update_myreferences'),
+    path('api/genealogy/update/<int:pk>/', views.updateGenealogy, name='update_genealogy'),
+    path('api/partnaire/update/<int:pk>/', views.updatePartnaire, name='update_partnaire'),
+    path('api/patronyme/update/<int:pk>/', views.updatePatronyme, name='update_patronyme'),
+    path('api/equivalence/update/<int:pk>/', views.updateEquivalence, name='update_equivalence'),
+    path('api/presentation/update/<int:pk>/', views.updatePresentation, name='update_presentation'),
+    path('api/blasonnements/update/<int:pk>/', views.updateBlasonnements, name='update_blasonnements'),
+
+
+    path('api/search/', views.search, name='search'),
     
 ]
--- palisep/base/views.py
+++ palisep/base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.db.models import Q
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
@@ -34,18 +34,17 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 
 
+@api_view(['GET'])
+def getBlasonnements(request):
+    blasonnements = Blasonnements.objects.all()
+    serializer = BlasonnementsSerializer(blasonnements, many=True)
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 def getCategory(request):
     category = Categories.objects.get()
     serializer = CategoriesSerializer(category)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getBlasonnements(request):
-    blasonnements = Blasonnements.objects.all()
-    serializer = BlasonnementsSerializer(blasonnements, many=True)
     return Response(serializer.data)
 
 
@@ -83,6 +82,42 @@ def getGenealogy(request):
     genealogy = Genealogy.objects.all()
     serializer = GenealogySerializer(genealogy, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getReferences(request):
+    reference = MyReferences.objects.all()
+    serializer = MyReferencesSerializer(reference, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getShots(request):
+    shot = Shots.objects.all()
+    serializer = ShotsSerializer(shot, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getDetails(request):
+    detail = Details.objects.all()
+    serializer = DetailsSerializer(detail, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getLegentPhotos(request):
+    LegentPhoto = LegentPhotos.objects.all()
+    serializer = LegentPhotosSerializer(LegentPhoto, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getArmorial(request):
+    armorial = Armorial.objects.all()
+    serializer = ArmorialSerializer(armorial, many=True)
+    return Response(serializer.data)
+
 
 # Delete APIs
 
@@ -190,3 +225,343 @@ def deleteAdmin(request, pk):
     admin = Admin.objects.get(id=pk)
     admin.delete()
     return Response("User Deleted")
+
+
+
+# Create Apis
+
+# @api_view(['POST'])
+# @permission_classes([IsAdminUser])
+# def createArmorial(request):
+#     if request.method == 'POST':
+#         serializer = ArmorialSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createBlasonnements(request):
+    if request.method == 'POST':
+        serializer = BlasonnementsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createCategories(request):
+    if request.method == 'POST':
+        serializer = CategoriesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createPresentation(request):
+    if request.method == 'POST':
+        serializer = PresentationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createEquivalence(request):
+    if request.method == 'POST':
+        serializer = EquivalenceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createPatronyme(request):
+    if request.method == 'POST':
+        serializer = PatronymeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createPartnaire(request):
+    if request.method == 'POST':
+        serializer = PartnaireSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createGenealogy(request):
+    if request.method == 'POST':
+        serializer = GenealogySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createReferences(request):
+    if request.method == 'POST':
+        serializer = MyReferencesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createShots(request):
+    if request.method == 'POST':
+        serializer = ShotsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createDetails(request):
+    if request.method == 'POST':
+        serializer = DetailsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createLegentPhotos(request):
+    if request.method == 'POST':
+        serializer = LegentPhotosSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createArmorial(request):
+    if request.method == 'POST':
+        serializer = ArmorialSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+# Update Apis
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateArmorial(request, pk):
+    try:
+        armorial = Armorial.objects.get(armorial_id=pk)
+    except Armorial.DoesNotExist:
+        return Response("Armorial not found", status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ArmorialSerializer(armorial, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateLegentPhotos(request, pk):
+    try:
+        legent_photo = LegentPhotos.objects.get(id=pk)
+    except LegentPhotos.DoesNotExist:
+        return Response("LegentPhoto not found", status=status.HTTP_404_NOT_FOUND)
+
+    serializer = LegentPhotosSerializer(legent_photo, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateDetails(request, pk):
+    try:
+        detail = Details.objects.get(id=pk)
+    except Details.DoesNotExist:
+        return Response("Details not found", status=status.HTTP_404_NOT_FOUND)
+
+    serializer = DetailsSerializer(detail, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateCategories(request, pk):
+    try:
+        category = Categories.objects.get(category_id=pk)
+    except Categories.DoesNotExist:
+        return Response("Category not found", status=status.HTTP_404_NOT_FOUND)
+
+    serializer = CategoriesSerializer(category, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateShots(request, pk):
+    try:
+        shot = Shots.objects.get(shot_id=pk)
+    except Shots.DoesNotExist:
+        return Response("Shot not found", status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ShotsSerializer(shot, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateMyReferences(request, pk):
+    try:
+        reference = MyReferences.objects.get(reference_id=pk)
+    except MyReferences.DoesNotExist:
+        return Response("Reference not found", status=status.HTTP_404_NOT_FOUND)
+
+    serializer = MyReferencesSerializer(reference, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateGenealogy(request, pk):
+    try:
+        genealogy = Genealogy.objects.get(id_genealogy=pk)
+    except Genealogy.DoesNotExist:
+        return Response("Genealogy not found", status=status.HTTP_404_NOT_FOUND)
+
+    serializer = GenealogySerializer(genealogy, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updatePartnaire(request, pk):
+    try:
+        partnaire = Partnaire.objects.get(id=pk)
+    except Partnaire.DoesNotExist:
+        return Response("Partnaire not found", status=status.HTTP_404_NOT_FOUND)
+
+    serializer = PartnaireSerializer(partnaire, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updatePatronyme(request, pk):
+    try:
+        patronyme = Patronyme.objects.get(id_patronym=pk)
+    except Patronyme.DoesNotExist:
+        return Response("Patronyme not found", status=status.HTTP_404_NOT_FOUND)
+
+    serializer = PatronymeSerializer(patronyme, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateEquivalence(request, pk):
+    try:
+        equivalence = Equivalence.objects.get(id=pk)
+    except Equivalence.DoesNotExist:
+        return Response("Equivalence not found", status=status.HTTP_404_NOT_FOUND)
+
+    serializer = EquivalenceSerializer(equivalence, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updatePresentation(request, pk):
+    try:
+        presentation = Presentation.objects.get(id=pk)
+    except Presentation.DoesNotExist:
+        return Response("Presentation not found", status=status.HTTP_404_NOT_FOUND)
+
+    serializer = PresentationSerializer(presentation, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateBlasonnements(request, pk):
+    try:
+        blasonnement = Blasonnements.objects.get(blasonnement_id=pk)
+    except Blasonnements.DoesNotExist:
+        return Response("Blasonnement not found", status=status.HTTP_404_NOT_FOUND)
+
+    serializer = BlasonnementsSerializer(blasonnement, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+ 
+
+
+# Search Functionality
+
+@api_view(['GET'])
+def search(request):
+    query = request.query_params.get('query', '')
+
+    # Perform a case-insensitive search on category names
+    categories_results = Categories.objects.filter(name__icontains=query)
+    categories_serializer = CategoriesSerializer(categories_results, many=True)
+
+    # Perform a case-insensitive search on armorial names
+    armorial_results = Armorial.objects.filter(Q(famille__icontains=query) | Q(country__icontains=query))
+    armorial_serializer = ArmorialSerializer(armorial_results, many=True)
+
+    # Perform a case-insensitive search on legentphotos titles and descriptions
+    legentphotos_results = LegentPhotos.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+    legentphotos_serializer = LegentPhotosSerializer(legentphotos_results, many=True)
+
+    # Perform a case-insensitive search on details titles
+    details_results = Details.objects.filter(title__icontains=query)
+    details_serializer = DetailsSerializer(details_results, many=True)
+
+    # Combine the results from different models into a single response
+    response_data = {
+        'categories': categories_serializer.data,
+        'armorial': armorial_serializer.data,
+        'legentphotos': legentphotos_serializer.data,
+        'details': details_serializer.data,
+    }
+
+    return Response(response_data)
